1119ca5bf4745bf6241c9d612b5720a2d8d80636
Andreas Brandt <Brandten@gmail.com>
2014-07-21 00:29:01 +0200

    can now upload a empty file.

--- src/upload.py
+++ src/upload.py
@@ -1,10 +1,10 @@
 import dropbox
-
+import sys
 client = ""
 
 def main():
     getAccessToken()
-    printClientInfo()
+    upload(sys.argv[1])
 
 
 
@@ -14,8 +14,10 @@ def getAccessToken():
         access_token = text_file.readline().rstrip()
         client = dropbox.client.DropboxClient(access_token)   
         
-def printClientInfo():
-    print client.account_info()
+def upload(uploadFile):
+    client.put_file("/"+uploadFile, uploadFile)
+    
+
 
 if __name__ == "__main__":
     main()
